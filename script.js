1a983c4f68b9106a1fdb4a0a29f84ee5231666d8
Risen7 <144148376+Risen7@users.noreply.github.com>
2024-12-20 20:20:26 +0800

    Update script.js v1.6

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// https://www.youtube.com/watch?v=hBbrGFCszU4 - drop down menu
+
 var selectedRow = null
 // var fud1 = document.getElementById("food-1").value;
 // var fud2 = document.getElementById("food-2").value;
@@ -35,8 +37,6 @@ function onFormSubmit() {
 
 
 // totalz = food1 + food2;
-
-
 
 
 function readFormData() {
@@ -206,3 +206,48 @@ function validate() {
     }
     return isValid;
 }
+
+//-----------------------------DROPDOWN-----------------------------------
+const dropdowns = document.querySelectorAll('.dropdown');
+
+//loop trough all dropdown elements
+dropdowns.forEach(dropdown => {
+    //Get inner elmeents from each dropdown
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelectorAll('.menu li');
+    const selected = dropdown.querySelector('.selected');
+
+
+    //add a circle event to the select element
+    select.addEventListener('click', () =>{
+        //add the clicked select styles to the select element
+        select.classList.toggle('select-clicked');
+        //add the rotate styles to the caret element
+        caret.classList.toggle('caret-rotate');
+        //add the open styles to the menu element
+        menu.classList.toggle('menu-open');
+    });
+
+    //loop through all option elements
+    options.forEach(option => {
+        //add a click event to the option element
+        option.addEventListener('click', () => {
+            //change selected inner text to clicked option inner text
+            selected.innerText = option.innerText;
+            //add the clicked select styles to the select element
+            select.classList.remove('select-clicked');
+            //add the rotate styles to the caret element
+            caret.classList.remove('caret-rotate');
+            //add the open styles to the menu element
+            menu.classList.remove('menu-open');
+            //remove active class from all option elements
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            //add active class to clicked option element
+            option.classList.add('active');
+        });
+    });
+});
